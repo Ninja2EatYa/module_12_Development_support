6cdb05b918296ee65220ef2b3aafa9d410947a06
Ninja2EatYa <alexandrov.peter@gmail.com>
2024-09-07 23:32:41 +0300

    Add files via upload

---
+++ 12_2_Проверка_на_выносливость_2.py
@@ -0,0 +1,43 @@
+import unittest
+import runner_and_tournament
+
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.hussein = runner_and_tournament.Runner('Усейн', 10)
+        self.andrey = runner_and_tournament.Runner('Андрей', 9)
+        self.nick = runner_and_tournament.Runner('Ник', 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for result in cls.all_results.values():
+            for place, runner in result.items():
+                print(f'{place}: {runner}', end='. ')
+            print('')
+
+
+    def test_start_1(self):
+        race = runner_and_tournament.Tournament(90, self.hussein, self.nick)
+        results = race.start()
+        self.all_results[1] = results
+        self.assertTrue(results[max(results.keys())] == self.nick)
+
+    def test_start_2(self):
+        race = runner_and_tournament.Tournament(90, self.andrey, self.nick)
+        results = race.start()
+        self.all_results[2] = results
+        self.assertTrue(results[max(results.keys())] == self.nick)
+
+    def test_start_3(self):
+        race = runner_and_tournament.Tournament(90, self.hussein, self.andrey, self.nick)
+        results = race.start()
+        self.all_results[3] = results
+        self.assertTrue(results[max(results.keys())] == self.nick)
+
+
+if __name__ == '__main__':
+    unittest.main()
