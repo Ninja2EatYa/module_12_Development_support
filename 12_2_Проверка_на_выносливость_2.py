a123dc251532e27f44d4a6bf4e7b68ba066d6293
Ninja2EatYa <alexandrov.peter@gmail.com>
2024-09-08 19:21:51 +0300

    12_2_Проверка_на_выносливость_2.py

--- 12_2_Проверка_на_выносливость_2.py
+++ 12_2_Проверка_на_выносливость_2.py
@@ -15,28 +15,29 @@ class TournamentTest(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         for result in cls.all_results.values():
-            for place, runner in result.items():
-                print(f'{place}: {runner}', end='. ')
-            print('')
+            print({place: str(runner) for place, runner in result.items()})
 
 
     def test_start_1(self):
         race = runner_and_tournament.Tournament(90, self.hussein, self.nick)
         results = race.start()
         self.all_results[1] = results
-        self.assertTrue(results[max(results.keys())] == self.nick)
+        runners = list(results.values())
+        self.assertTrue(runners[-1] == self.nick)
 
     def test_start_2(self):
         race = runner_and_tournament.Tournament(90, self.andrey, self.nick)
         results = race.start()
         self.all_results[2] = results
-        self.assertTrue(results[max(results.keys())] == self.nick)
+        runners = list(results.values())
+        self.assertTrue(runners[-1] == self.nick)
 
     def test_start_3(self):
         race = runner_and_tournament.Tournament(90, self.hussein, self.andrey, self.nick)
         results = race.start()
         self.all_results[3] = results
-        self.assertTrue(results[max(results.keys())] == self.nick)
+        runners = list(results.values())
+        self.assertTrue(runners[-1] == self.nick)
 
 
 if __name__ == '__main__':
