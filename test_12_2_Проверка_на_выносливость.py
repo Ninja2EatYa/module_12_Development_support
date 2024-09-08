92b3953cb659dbafdcbb67e77dbc34233a8c0b57
Ninja2EatYa <alexandrov.peter@gmail.com>
2024-09-08 19:18:09 +0300

    test_12_2_Проверка_на_выносливость.py

--- test_12_2_Проверка_на_выносливость.py
+++ test_12_2_Проверка_на_выносливость.py
@@ -17,30 +17,30 @@ class TournamentTest(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         for result in cls.all_results.values():
-            for place, runner in result.items():
-                print(f'{place}: {runner}', end='. ')
-            print('')
+            print({place: str(runner) for place, runner in result.items()})
 
     @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_start_1(self):
         race = runner_and_tournament.Tournament(90, self.hussein, self.nick)
         results = race.start()
         self.all_results[1] = results
-        self.assertTrue(results[max(results.keys())] == self.nick)
+        runners = list(results.values())
+        self.assertTrue(runners[-1] == self.nick)
 
     @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_start_2(self):
         race = runner_and_tournament.Tournament(90, self.andrey, self.nick)
         results = race.start()
         self.all_results[2] = results
-        self.assertTrue(results[max(results.keys())] == self.nick)
+        runners = list(results.values())
+        self.assertTrue(runners[-1] == self.nick)
 
     @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_start_3(self):
         race = runner_and_tournament.Tournament(90, self.hussein, self.andrey, self.nick)
         results = race.start()
-        self.all_results[3] = results
-        self.assertTrue(results[max(results.keys())] == self.nick)
+        runners = list(results.values())
+        self.assertTrue(runners[-1] == self.nick)
 
 
 if __name__ == '__main__':
